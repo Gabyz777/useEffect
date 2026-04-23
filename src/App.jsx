0a87b13b02e8228b7fcbc22b22a358622eca92e4
Gabyz777 <gabriellydiasmendes@gmail.com>
2026-04-23 14:12:15 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.module.css'
+import { Desafio } from './components/Desafio'
+
+export default function App() {
+  return (
+    <>
+      <h1>Desafio useEffect</h1>
+      <Desafio />
+    </>
+  )
+}
---
+++ src/components/Desafio.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react'
+import styles from './Desafio.module.css'
+
+export function Desafio() {
+  const [senha, setSenha] = useState("")
+  const [tentativas, setTentativas] = useState(0)
+  const [bloqueado, setBloqueado] = useState(false)
+
+  // 1. Efeito de Inicialização
+  useEffect(() => {
+    alert("SISTEMA INICIALIZADO: Aguardando credenciais...")
+  }, [])
+
+  // 2. Efeito de Validação em Tempo Real
+  useEffect(() => {
+    if (senha === "ADMIN123") {
+      alert("ACESSO CONCEDIDO!")
+    }
+  }, [senha])
+
+  // 3. Efeito de Bloqueio
+  useEffect(() => {
+    if (tentativas >= 3) {
+      setBloqueado(true)
+      setSenha("")
+      alert("SISTEMA BLOQUEADO: Tentativas excessivas!!")
+    }
+  }, [tentativas])
+
+  function validarAcesso() {
+    if (senha !== "ADMIN123") {
+      setTentativas(prev => prev + 1)
+      alert("ACESSO NEGADO!")
+    }
+  }
+
+
+  return (
+    <div className={styles.corpo}>
+      <div className={styles.terminal}>
+        <h1 className={styles.titulo}>SECURITY LOGIN</h1>
+        
+        <p className={styles.status}>
+          Tentativas de erro: <span className={styles.erro}>{tentativas} / 3</span>
+        </p>
+
+        <input 
+          type="password" 
+          placeholder="Digite a senha de acesso..."
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          disabled={bloqueado}
+          className={styles.inputSenha}
+        />
+
+        <button 
+          onClick={validarAcesso} 
+          disabled={bloqueado}
+          className={styles.botao}
+        >
+          {bloqueado ? "SISTEMA TRAVADO" : "TENTAR ACESSO"}
+        </button>
+
+        {bloqueado && (
+          <p className={styles.aviso}>Contate o administrador do sistema.</p>
+        )}
+      </div>
+    </div>
+  )
+}
